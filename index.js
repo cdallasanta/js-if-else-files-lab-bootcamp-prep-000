a9830959e6c76d004b4c1c10bfa15153f309e9de
Chris Dalla Santa <chris.dallasanta@gmail.com>
2018-11-16 04:58:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-var name = "Joe";
-var height = "74";
-/*
-if(name === 'Susan'){
-  console.log("The name is correct");
-} else {
-  console.log("Expected the name to be Susan");
-}
-*/
-// Don't worry about this
-module.exports = { name, height
-};
-
+var index = require("./index.js")
+ 
+console.log(index.name)
